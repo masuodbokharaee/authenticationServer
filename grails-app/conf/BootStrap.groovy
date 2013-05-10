d0a629503eb698ec8cacd59f57b3c596489a1d77
masuod <masuodbokharaee@gmail.com>
2013-05-10 13:13:30 +0430

    authenticationServer

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,7 +3,7 @@ import asSecurity.User
 class BootStrap {
 
     def init = { servletContext ->
-        def user = User.findByUsername("demo") ?: new User(username: "demo", password: "demo", enabled: true).save()
+        def user = User.findByUsername("admin") ?: new User(username: "admin", password: "admin", enabled: true).save()
     }
     def destroy = {
     }
